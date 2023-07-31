dddb59b8c9e8b0b8e8613ce45b3544f08fa31dfd
llf <2367746876@qq.com>
2023-07-31 18:22:24 +0800

    demo for Vue3 in Vite

---
+++ vue-project/src/views/DemoView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an demo page</h1>
+    <button @click="change">{{ store.count }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
+import { useCounterStore } from '@/stores/counter'
+const store = useCounterStore()
+
+// const { count } = storeToRefs(store)
+// console.log(store)
+function change() {
+  store.increment()
+}
+
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
